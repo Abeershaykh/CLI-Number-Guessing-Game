d193e2ce33a45756b7ad282c5d098f35ddac6ee6
Abeer Shaikh <shaikhabeer4@gmail.com>
2024-04-02 04:25:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 3 + 2);
+console.log("Welcome to Number Guessing Game");
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-6: ",
+    },
+]);
+if (answers.UserGuessedNumber === randomNumber) {
+    console.log(`Congratulations! You guessed correct number.`);
+}
+else {
+    console.log(`You guessed wrong number.`);
+}
